ddfff394b1619cba96241027cefa3d6db972b8e4
Adrian Chong <adrian.chong@lightspeedretail.com>
2017-05-14 23:10:55 -0400

    schema

--- schema.py
+++ schema.py
@@ -1,1 +1,14 @@
-hello
+from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer, DECIMAL
+
+Base = declarative_base()
+class Car(Base):
+    __tablename__ = 'car'
+    id = Column(Integer, primary_key = True)
+    year = Column(Integer)
+    selling_price = Column(DECIMAL)
+
+engine = create_engine('sqlite:///car_finder.db')
+Base.metadata.create_all(engine)
